c178638dc24d6b7f13e0bfe4f9d9c5c6eac3d259
Äntuchakma <biposhyan@gmail.com>
2026-04-08 03:29:00 +0600

    initial commit

---
+++ cognify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cognify
+//
+//  Created by Nahian Zarif on 8/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cognify/cognifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  cognifyApp.swift
+//  cognify
+//
+//  Created by Nahian Zarif on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct cognifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
